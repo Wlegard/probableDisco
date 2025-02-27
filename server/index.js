a021f7e9557061a4976c0c7d579f8f52ccc48e7f
PeytonStrahan <peytonstrahan@outlook.com>
2025-02-27 12:03:48 -0600

    add routes and create working express server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const path = require("path");
+
+// import routes
+const avatarRoute = require("./routes/avatar.js");
+const commentsRoute = require("./routes/comments.js");
+const libraryRoute = require("./routes/library.js");
+const queueRoute = require("./routes/queue.js");
+const settingsRoute = require("./routes/settings.js");
+
+// create express app
 const application = express();
+// select port number
 const portNum = 3000;
 
+// middleware
+application.use(express.json());
+application.use(express.urlencoded({extended: true}));
+
+// serve up static files (react app)
+const distPath = path.resolve(__dirname, '../dist');
+application.use(express.static(distPath));
+
+// start up the express server using the port number
 application.listen(portNum, () => {
   console.log('Listening on port: ' + portNum)
 })
---
+++ server/routes/comments.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const route = express.Router();
+
+// handle GET requests
+route.get('/', (req, res) => {
+
+});
+
+// handle POST requests
+route.post('/', (req, res) => {
+
+});
+
+// handle PATCH requests
+route.patch('/', (req, res) => {
+
+});
+
+// handle DELETE requests
+route.delete('/', (req, res) => {
+
+});
+
+// export the route for use in server/index.js
+module.exports = route;
