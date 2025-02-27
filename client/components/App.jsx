48a2139248324f8c69e9851284fd9ff35003f79d
PeytonStrahan <peytonstrahan@outlook.com>
2025-02-27 15:12:19 -0600

    render App component

---
+++ client/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor () {
+    super();
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>placeholder</div>
+    )
+  }
+};
+
+export default App;
