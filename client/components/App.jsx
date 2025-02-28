e1b0fd073782e0b4551d5ac82337ab344d7dc425
charles <thereal397@gmail.com>
2025-02-28 09:26:24 -0600

    set state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      
-    }
+        avatarImages: [],
+        currentImage: ''
+    };
+    this.stateChanger = this.stateChanger.bind(this);
   }
 
-  componentDidMount() {
 
-  }
+
+  componentDidMount() {}
+
+stateChanger(stateName, dataToInsert){
+    this.setState({
+        [stateName]: dataToInsert
+    })
+}
 
   render() {
-    return (
-      <div>placeholder</div>
-    )
+    return <div>placeholder</div>;
   }
-};
+}
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
 
 dotenv.config({
-  path: path.resolve(__dirname, '../.env')
-})
+  path: path.resolve(__dirname, "../.env"),
+});
 
 // import routes
-const avatarRoute = require('./routes/avatar');
-const commentsRoute = require('./routes/comments');
-const libraryRoute = require('./routes/library');
-const queueRoute = require('./routes/queue');
-const settingsRoute = require('./routes/settings');
+const avatarRoute = require("./routes/avatar");
+const commentsRoute = require("./routes/comments");
+const libraryRoute = require("./routes/library");
+const queueRoute = require("./routes/queue");
+const settingsRoute = require("./routes/settings");
 
 // create express app
 const application = express();
@@ -23,7 +23,7 @@ application.use(express.json());
 application.use(express.urlencoded({ extended: true }));
 
 // serve up static files (react app)
-const distPath = path.resolve(__dirname, '../dist');
+const distPath = path.resolve(__dirname, "../dist");
 application.use(express.static(distPath));
 
 // start up the express server using the port number
