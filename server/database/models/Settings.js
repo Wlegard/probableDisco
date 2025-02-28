eaa48c2c844db810ee972be443e42b1246d9f219
PeytonStrahan <peytonstrahan@outlook.com>
2025-02-27 19:56:30 -0600

    add onto settings db schema and request handlers

--- server/database/models/Settings.js
+++ server/database/models/Settings.js
@@ -4,8 +4,19 @@ const { Schema, model } = mongoose;
 
 const SettingsSchema = new Schema(
   {
+    loadoutName:
+    appSetting: {
+      primaryColor: String,
+      secondaryColor: String,
 
-  }
+    },
+    avatarSettings: {},
+    commentsSettings: {},
+    librarySettings: {},
+    queueSettings: {},
+    settingsSettings: {},
+    userId: Number,
+  },
 );
 
 const Settings = model('Settings', SettingsSchema);
