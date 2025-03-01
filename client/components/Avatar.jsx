5c50e91e6faeea8dca64f29be8d572f9c5045917
charles <thereal397@gmail.com>
2025-03-01 16:00:13 -0600

    file uploader & avatar

--- client/components/Avatar.jsx
+++ client/components/Avatar.jsx
@@ -1,1 +1,15 @@
-const avatar = 
+import React, { useState, useEffect } from "react";
+
+const axios = require("axios");
+
+function Avatar(props) {
+  
+
+  return (
+    <div>
+     <img src={props.currentImage}/>
+    </div>
+  );
+}
+
+export default Avatar;
