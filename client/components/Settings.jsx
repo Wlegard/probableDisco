e4f4d19fd7937fb172b317d60dd67b08b7a5f533
PeytonStrahan <peytonstrahan@outlook.com>
2025-03-03 12:01:55 -0600

    finish Settings Schema, begin Settings routes and route callers starting with GET

--- client/components/Settings.jsx
+++ client/components/Settings.jsx
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from 'react';
 const axios = require('axios');
 
-function Settings(props) {
+function Settings({ themes, setThemes, theme, setTheme}) {
   //const [something, setSomething] = useState(0);
+
+  useEffect(() => {
+    // run setTheme to change the application's current theme upon first render of the Settings component
+    setTheme(themes[0]);
+  }, []);
+
   const getThemes = () => {
-    axios.get('/settings');
+    // send get request to /settings on server
+    axios.get('/settings')
+      .then(settings => {
+        // set themes in App's state to be the returned settings
+        setThemes(settings);
+      })
+      .catch(err => {
+        // handle errors
+        console.error("Failed to get theme settings from database", err);
+      });
   };
   const addTheme = () => {
     axios.post('/settings');
@@ -15,6 +30,10 @@ function Settings(props) {
   const updateTheme = () => {
     axios.patch('/settings');
   };
+  // const setCurrentTheme = (selectedTheme) => {
+  //   // set the theme in App's state to be the theme you want the application to currently be using
+  //   setTheme(selectedTheme);
+  // }
 
 
   return (
@@ -26,14 +45,27 @@ function Settings(props) {
             <div key="idk">
               <option key="idk">Example Theme</option>
               <button>Delete Theme</button>
+              <button>Use Theme</button>
             </div>
           }
         </select>
         <div id="forms">
           {
             <div key="idk">
+              <span style={{color:'darkgrey'}}>Theme Name</span>
+              <input onChange={e => console.log(e.target.value)} placeholder="insert the name of your theme"></input>
               <span style={{color:'darkgrey'}}>Primary Color</span>
               <input onChange={e => console.log(e.target.value)} placeholder="insert primary color (can be name or hex value)"></input>
+              <span style={{color:'darkgrey'}}>Secondary Color</span>
+              <input onChange={e => console.log(e.target.value)} placeholder="insert secondary color (can be name or hex value)"></input>
+              <span style={{color:'darkgrey'}}>Tertiary Color</span>
+              <input onChange={e => console.log(e.target.value)} placeholder="insert tertiary color (can be name or hex value)"></input>
+              <span style={{color:'darkgrey'}}>Custom cursor image</span>
+              <input onChange={e => console.log(e.target.value)} placeholder="insert image link for you mouse cursor"></input>
+              <span style={{color:'darkgrey'}}>Font</span>
+              <input onChange={e => console.log(e.target.value)} placeholder="insert name of desired font"></input>
+              <span style={{color:'darkgrey'}}>Corner Rounding</span>
+              <input onChange={e => console.log(e.target.value)} placeholder="insert number (of pixels) you want to round the corners of boxes by"></input>
             </div>
           }
           <button>Add Theme</button>
--- server/routes/settings.js
+++ server/routes/settings.js
@@ -2,17 +2,38 @@ const express = require('express');
 
 // import Settings db model
 const { Settings } = require('../database/index');
-
+// create router
 const route = express.Router();
+
+// create reference to default values?
 
 // handle GET requests
 route.get('/settings', (req, res) => {
-
+  // acquire array of settings (themes) from database
+  Settings.find()
+    .then(themes => {
+      // send found array of themes back with status of 200
+      res.status(200).send(themes);
+    })
+    .catch(err => {
+      // handle errors by sending back status of 500
+      res.sendStatus(500);
+    })
 });
 
 // handle POST requests
 route.post('/settings', (req, res) => {
 
+  // insert new setting (theme) into the database
+  Settings.create(req.body)
+    .then(() => {
+      // send status of 201 upon successful creation of new theme in settings database
+      res.sendStatus(201);
+    })
+    .catch(err => {
+      // handle errors by sending back status of 500
+      res.sendStatus(500);
+    })
 });
 
 // handle PATCH requests
